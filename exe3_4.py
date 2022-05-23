bd3c898089fbdb65dbef33a46b50d70c6a52aee9
KumudPatil <103893043+KumudPatil@users.noreply.github.com>
2022-05-23 21:56:36 +0530

    Add files via upload

---
+++ exe3_4.py
@@ -0,0 +1,4 @@
+l=float(input("enter the leagth:"))
+b=float(input("enter the breadth:"))
+area=l*b
+print("area of rectangle is: %2f"%area)
---
+++ exe3_5.py
@@ -0,0 +1,5 @@
+side=int(input("side:"))
+area=side*side
+perimeter=4*side
+print("Area of Rectangle :",area)
+print("Perimeter of Rectangle :",perimeter)
